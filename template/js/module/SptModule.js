2d23c0f1dd47f5d98b458388dd7dec17851466ce
Kyle <kyle.kiet@namtech.sg>
2023-08-22 13:47:26 +0700

    create page blog

--- template/js/module/SptModule.js
+++ template/js/module/SptModule.js
@@ -1,1 +1,16 @@
-export default function SptModule() {}
+export default function SptModule() {
+    $(document).ready(function() {
+        $(".fblog-inner .fblog-img").eq(0).addClass("show"); 
+        $(".fblog-link-js").hover(
+            function() {
+                // Sử dụng $(this) để xác định phần tử liên quan
+                $(".fblog-inner .fblog-img").eq(0).removeClass("show"); 
+                $(this).parent(".fblog-gr").parent(".fblog-inner").find(".fblog-img").addClass("show");
+            },
+            function() {
+                $(this).parent(".fblog-gr").parent(".fblog-inner").find(".fblog-img").removeClass("show");
+                $(".fblog-inner .fblog-img").eq(0).addClass("show"); 
+            }
+          );
+      });
+}
