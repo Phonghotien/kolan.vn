537732c64e1002437742b9969564b1343599c98d
Phonghotien <hotienphong120699@gmail.com>
2023-08-30 17:36:13 +0700

    phong-update

--- template/js/module/BtnToTopModule.js
+++ template/js/module/BtnToTopModule.js
@@ -1,23 +1,47 @@
 export default function BtnToTopModule() {
-    const btnToTop = document.querySelector(".backToTop");
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 10) {
-            btnToTop.classList.add("active");
-        } else {
-            btnToTop.classList.remove("active");
-        }
-    });
-    if (btnToTop) {
-        btnToTop.addEventListener("click", function() {
-            document.body.scrollTop = 0;
-            document.documentElement.scrollTop = 0;
-        });
+  const btnToTop = document.querySelector(".backToTop");
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 10) {
+      btnToTop.classList.add("active");
+    } else {
+      btnToTop.classList.remove("active");
     }
-    $(document).ready(function() {
-        if (window.scrollY > 10) {
-            btnToTop.classList.add("active");
-        } else {
-            btnToTop.classList.remove("active");
-        }
+  });
+  if (btnToTop) {
+    btnToTop.addEventListener("click", function () {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
     });
+  }
+  $(document).ready(function () {
+    if (window.scrollY > 10) {
+      btnToTop.classList.add("active");
+    } else {
+      btnToTop.classList.remove("active");
+    }
+  });
+  const speed = 300;
+  // NẾU CÓ ĐỊA CHỈ ID TRÊN THANH URL THÌ SCROLL XUỐNG
+  const hash = window.location.hash;
+  if ($(hash).length) scrollToID(hash, speed);
+  // TÌM ĐỊA CHỈ ID VÀ SCROLL XUỐNG NẾU CÓ CLASS
+  $(".pdp-thumb .scroll-button a").on("click", function (e) {
+    e.preventDefault();
+
+    const href = $(this).find("> a").attr("href") || $(this).attr("href");
+    const id = href.slice(href.lastIndexOf("#"));
+    if ($(id).length) {
+      scrollToID(id, speed);
+    } else {
+      // window.location.replace(/${id});
+      window.location.href = href;
+    }
+  });
+  // HÀM SCROLL CHO MƯỢT MÀ
+  function scrollToID(id, speed) {
+    const offSet = $(".header").outerHeight();
+    const section = $(id).offset();
+    const targetOffset = section.top - offSet - 0;
+    $("html,body").animate({ scrollTop: targetOffset }, speed);
+  }
 }
--- template/js/module/SwiperModule.js
+++ template/js/module/SwiperModule.js
@@ -1,3 +1,29 @@
 export default function SwiperModule() {
+  const home_silder = document.querySelectorAll(".bn-slider");
+  if (home_silder) {
+    home_silder.forEach((item, i) => {
+      const swiperMain = item.querySelector(".bn-main .swiper");
+      const swiperThumb = item.querySelector(".bn-thumb .swiper");
 
+      const itemText = new Swiper(swiperThumb, {
+        speed: 800,
+        slideToClickedSlide: true,
+        slidesPerView: "auto",
+      });
+      const itemMain = new Swiper(swiperMain, {
+        autoplay: {
+          delay: 3000,
+        },
+        speed: 800,
+        slidesPerView: 1,
+        effect: "fade",
+        fadeEffect: {
+          crossFade: true,
+        },
+        thumbs: {
+          swiper: itemText,
+        },
+      });
+    });
+  }
 }
